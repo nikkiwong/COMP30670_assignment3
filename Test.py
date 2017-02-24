1a81e9f6931b554f7d48838e60156b7c567dcde3
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-02-24 12:49:24 +0000

    skeleton

--- Test.py
+++ Test.py
@@ -5,7 +5,19 @@ from led_file import *
 from setup import read_file
 
 def testing():
-    filename =
-    buffer = read_file(filename=filename)
+    filename = "C:\Users\Nikki\Desktop\CS UCD\software engineering\/assignment_3\/testfile.txt"
+    buffer = read_file(filename == filename)
     eq_(len(buffer), 0, "The values do not match for the buffer.")
+
+def testing_line():
+    pass
+
+def testing_line2():
+    pass
+
+def tester_run():
+    pass
+
+def test_count():
+    pass
 
