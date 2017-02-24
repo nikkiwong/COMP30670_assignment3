acb56372b9587bdc1945e55cc3d483b0e8aede2b
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-02-24 12:37:24 +0000

    test

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
 #this is where I will write all my test codes
+
+from nose.tools import *
+from led_file import *
+from setup import read_file
+
+def testing():
+    filename =
+    buffer = read_file(filename=filename)
+    eq_(len(buffer), 0, "The values do not match for the buffer.")
+
--- setup.py
+++ setup.py
@@ -1,1 +1,4 @@
 #this is where I will write my setup.py
+
+def read_file():
+    pass
