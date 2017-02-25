18a30e0394592db8cc178d24d2cc7cc03e76b4ed
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-02-25 07:59:54 +0000

    trying to write a functioning main.py file

--- Comp30670_assignment3/Test_unittest.py
+++ Comp30670_assignment3/Test_unittest.py
@@ -3,15 +3,36 @@ import unittest
 class FooTests(unittest.TestCase):
 
     def setUp(self):
-        self.lights = lightOn(a, b, c, d)
+        self.lights = Lights(a, b, c, d)
 
     def light_status_match(self):
+        self.failUnless(False)
+        self.failIf(False)
+
+    def test_count(self):
+        self.failUnless(False)
+        self.failIf(False)
+
+    def test_turnOn(self):
+        self.failUnless(False)
+        self.failIf(False)
+
+    def test_turnOff(self):
+        self.failUnless(False)
+        self.failIf(False)
+
+    def test_switch(self):
         self.failUnless(False)
         self.failIf(False)
 
     def read_file_match(self):
         self.failUnless(False)
         self.failIf(False)
+
+    def test_read_file(self):
+        self.failUnless(False)
+        self.failIf(False)
+
 
 def main():
     unittest.main()
--- Comp30670_assignment3/led_file.py
+++ Comp30670_assignment3/led_file.py
@@ -1,2 +1,9 @@
 #where I will write the codes for the
-#
+class Lights:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def matches(self, ):
