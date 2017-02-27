df22699303f0984be0566cecf238681296db0c40
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-02-27 23:33:46 +0000

    trying to get the lights turn off to work.

--- src/Test_unittest.py
+++ src/Test_unittest.py
@@ -15,22 +15,24 @@ class LED_Tests(unittest.TestCase):
         self.assertEqual(C, counts, "The lights on count does not match")
         
     def test_TurnOnWorks(self):
-        on = Lights(5).turnOn(2,2,3,4)
+        light = Lights(5)
+        on = light.turnOn(2,2,3,4)
         counts = np.count_nonzero(on)
         expected = 6
         self.assertEqual(counts, expected, "Number of lights turned on didn't match")
 
     def test_TurnOffWorks(self):
-        on = Lights(5).turnOn(2,2,3,4)
-        print(on)
-        off = Lights.turnOff(on, 1,3,2,4)
+        light = Lights(5)
+        A = light.turnOn(2,2,3,4)
+        A = light.turnOff(1,3,2,4)
         counts = np.size(off) - np.count_nonzero(off)
         expected = 25
         self.assertEqual(counts, expected, "Number of lights turned off didn't match")
 
     def test_SwitchWorks(self):
-        on = Lights(5).turnOn(2,2,3,4)
-        sw = Lights(5).switch()
+        light = Lights(5)
+        on = light.turnOn(2,2,3,4)
+        sw = light.switch(on)
         counts = np.size(sw) - np.count_nonzero(sw)
         expected=0
         self.assertEqual(counts, expected, "Number of lights switched didn't match")
