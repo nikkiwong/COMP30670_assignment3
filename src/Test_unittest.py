0a45d43e57e66ca89f7d468cbb2c2653cc005203
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-02-28 00:37:32 +0000

    all tests working

--- src/Test_unittest.py
+++ src/Test_unittest.py
@@ -1,5 +1,6 @@
 from led_file import Lights
 import unittest
+from main import read_uri
 import numpy as np
 
 
@@ -8,7 +9,11 @@ class LED_Tests(unittest.TestCase):
     A = np.array(grid)
     # def setUp(self):
         # self.lights = Lights(a, b, c, d)
-
+    def test_read(self):
+        filename = "../data/input_assign3.txt"
+        buffer = read_uri(filename=filename)
+        self.assertEqual(len(buffer), 9506, "buffer size do not match")
+        
     def test_CountMatch(self):
         C = Lights(5).count()
         counts = np.count_nonzero(LED_Tests.A)
@@ -16,8 +21,8 @@ class LED_Tests(unittest.TestCase):
         
     def test_TurnOnWorks(self):
         light = Lights(5)
-        on = light.turnOn(2,2,3,4)
-        counts = np.count_nonzero(on)
+        A = light.turnOn(2,2,3,4)
+        counts = np.count_nonzero(A)
         expected = 6
         self.assertEqual(counts, expected, "Number of lights turned on didn't match")
 
@@ -25,22 +30,19 @@ class LED_Tests(unittest.TestCase):
         light = Lights(5)
         A = light.turnOn(2,2,3,4)
         A = light.turnOff(1,3,2,4)
-        counts = np.size(off) - np.count_nonzero(off)
-        expected = 25
+        counts = np.size(A) - np.count_nonzero(A)
+        expected = 21
         self.assertEqual(counts, expected, "Number of lights turned off didn't match")
 
     def test_SwitchWorks(self):
         light = Lights(5)
-        on = light.turnOn(2,2,3,4)
-        sw = light.switch(on)
-        counts = np.size(sw) - np.count_nonzero(sw)
-        expected=0
+        A = light.turnOn(2,2,3,4)
+        A = light.switch(1,3,2,4)
+        counts = np.count_nonzero(A)
+        expected=6
         self.assertEqual(counts, expected, "Number of lights switched didn't match")
 
-#     def test_ReadFileMatch(self):
-#         fm = mainfile.read_uri
-#         self.assertEqual(fm, 0, "The file wasn't read correctly")
-# 
+ 
 #     def test_ReadFileNotMatch(self):
 #         fm = mainfile.read_uri
 #         self.assertNotEqual(fm, 0, "The file wasn't read correctly")
