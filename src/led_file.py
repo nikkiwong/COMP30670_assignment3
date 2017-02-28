fb7a9093722ec9617e4146e11b61222674b3ce1b
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-02-28 00:49:15 +0000

    trying to get the class to work in the main file

--- src/led_file.py
+++ src/led_file.py
@@ -6,6 +6,9 @@ class Lights:
         grid = [[False] * self.size for _ in range(self.size)]
         self.A = np.array(grid)
         
+    def grid(self):
+        return self.A
+    
     def count(self):
         # [row.count(True) for row in self.grid]
         lightsOnCount = np.count_nonzero(self.A)
@@ -24,10 +27,10 @@ class Lights:
         return self.A
 
 
-    def switch(self):
-        for i in range(0, len(self.A)):
-            for j in range(0, len(self.A)):
-                self.A[i][j]= not self.A[i][j]
+    def switch(self, x1, y1, x2, y2):
+        for row in range(y1, y2+1):
+            for col in range(x1, x2+1):
+                self.A[row][col]= not self.A[row][col]
         return self.A
 
 # grid = Lights(10).create_grid()
--- src/main.py
+++ src/main.py
@@ -1,21 +1,17 @@
 import argparse
-import sys
 import urllib.request
 import numpy as np
+from led_file import Lights
 # help(urllib.request.urlopen)
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', help='input help')
-#     parser.add_argument('--kind', help='class type')
-    args = parser.parse_args()
-    filename = args.input
+def read_uri(filename="input_assign3"):
+    if filename.startswith('http'):
+        return urllib.request.urlopen(filename).read().decode('utf-8')       
+    else:
+        return open(filename).read()
     
+def parse(filename):
     buffer = read_uri(filename=filename)
-#     
-#     lines = buffer.split('\n')
-#     size = int(lines[0])
-#     print(lines)
 
     newfile = []
     plaintext = buffer.replace(',', ' ')
@@ -28,13 +24,39 @@ def main():
         newfile.append(values)     
     A=np.array(newfile)
     print(A)
+    size = int(A[0][0])
+    light = Lights(size)
+    print(len(A))
+    for i in range (1, len(A)-2):
+        cmd = A[i][0]
+        print(cmd)
+        x1 = int(A[i][1])
+        print(x1)
+        y1 = int(A[i][2])
+        print(y1)
+        x2 = int(A[i][3])
+        print(x2)
+        y2 = int(A[i][4])
+        print(y2)
+        if cmd == 'turn on':
+            B = light.turnOn(x1, y1, x2, y2)
+        elif cmd == 'turn off':
+            B = light.turnOff(x1, y1, x2, y2)
+        elif cmd == 'switch':
+            B = light.switch(x1, y1, x2, y2)
+
+    return B
     
-         
-def read_uri(filename="input_assign3"):
-    if filename.startswith('http'):
-        return urllib.request.urlopen(filename).read().decode('utf-8')       
-    else:
-        return open(filename).read()
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='input help')
+#     parser.add_argument('--kind', help='class type')
+    args = parser.parse_args()
+    filename = args.input
+    f = parse(filename)
+    print(f)
+    print("hello")
 
 if __name__ == '__main__':
     main()
